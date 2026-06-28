3e3c9b59126ff2b9b3cade0abb5f06b1f25854d3
Павел Ещик <eshchik_pm@ascon.ru>
2026-06-28 19:18:53 +0300

    removed index.test.js

--- test/commands/diplodoc-helper.links.coder.test.js
+++ test/commands/diplodoc-helper.links.coder.test.js
@@ -8,5 +8,10 @@ describe('diplodoc-helper.links.coder', () => {
     test('encodePathSegments should be defined', () => {
         expect(diplodoc_helper_links_coder.encodePathSegments).toBeDefined();
     });
+    test('encodePath sample 1', () => {
+        const value = diplodoc_helper_links_coder.encodePathSegments('loremИпсум');
+        expect(value).toEqual('lorem%D0%98%D0%BF%D1%81%D1%83%D0%BC');
+    });
+
     test.todo('encodePathSegments should work correctly');
 });
